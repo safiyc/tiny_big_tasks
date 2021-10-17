82317326750cba5e3c07999ea8cd4e01b43269a8
Safiy Cham <csafiy@yahoo.com>
2021-10-17 09:01:06 -0700

    User can delete a task from any of the list of columns, affecting both client and server

--- src/App.js
+++ src/App.js
@@ -33,10 +33,11 @@ const App = () => {
   const [items, setItems] = useState([]);
   const [newItem, setNewItem] = useState('');
 
+  // add item/task to server and client
   const addItem = (e) => {
     e.preventDefault();
 
-    // grab checked radio status value
+    // grab checked radio status value (Today, Tomorrow, Soon)
     const itemStatus = document.querySelector('input[name="status"]:checked').value;
 
     const newTask = {
@@ -44,16 +45,44 @@ const App = () => {
       "status": itemStatus
     };
 
-    console.log('new item: ', newTask);
+    // console.log('new item: ', newTask);
 
+    // add new item to server, then
     axios.post('http://localhost:3001/tasks', newTask).then(response => {
+      // add new item (response.data) to items arr and update state
       setItems(items.concat(response.data));
-      console.log('items list: ', items); // will not display new item; behind by one index
+      // console.log('items list: ', items); // new item will display on page, but not console log; behind by one index bc process is asynchronous
+
+      // empty input field of task
+      setNewItem('');
     });
+
+    // console.log('items list: ', items); // new item will display on page, but not console log; behind by one index bc process is asynchronous
   };
 
+  // delete selected task from server and client
+  const deleteTask = (id) => {
+    // find matching id, then return item/task
+    const taskToDelete = items.find(item => item.id === id);
+    // console.log('task to delete: ', taskToDelete);
+
+    // delete from server, then
+    axios.delete(`http://localhost:3001/tasks/${taskToDelete.id}`).then(() => {
+      // return arr of items/tasks that dont match the id of the deleted task
+      const updatedList = items.filter(item => item.id !== taskToDelete.id);
+      // console.log('tasks after server delete: ', updatedList);
+
+      // rerender client with updated state of tasks
+      setItems(updatedList);
+      // console.log('tasks after client delete: ', items);  // console will not log bc setItems is asynchronous; but client will update
+    });
+
+    // console.log('tasks after client delete: ', items);  // console will log update bc axios promise is asynchronous; client will update
+  };
+
+  // grab realtime state of task desc input field
   const handleChangeItemName = (e) => {
-    console.log(e.target.value);
+    // console.log(e.target.value);
     setNewItem(e.target.value);
   };
 
@@ -91,7 +120,7 @@ const App = () => {
           <h4 style={centerText}>Today</h4>
           <ul>
             {items.filter(item => item.status === "Today").map(item =>
-              <li key={item.task}>{item.task} - {item.status}</li>
+              <li key={item.task}>{item.task} - {item.status} --- <button onClick={() => deleteTask(item.id)}>Delete</button></li>
             )}
           </ul>
         </div>
@@ -99,7 +128,7 @@ const App = () => {
           <h4 style={centerText}>Tomorrow</h4>
           <ul>
             {items.filter(item => item.status === 'Tomorrow').map(item =>
-              <li key={item.task}>{item.task} - {item.status}</li>
+              <li key={item.task}>{item.task} - {item.status} --- <button onClick={() => deleteTask(item.id)}>Delete</button></li>
             )}
           </ul>
         </div>
@@ -107,7 +136,7 @@ const App = () => {
           <h4 style={centerText}>Soon</h4>
           <ul>
             {items.filter(item => item.status === 'Soon').map(item =>
-              <li key={item.task}>{item.task} - {item.status}</li>
+              <li key={item.task}>{item.task} - {item.status} --- <button onClick={() => deleteTask(item.id)}>Delete</button></li>
             )}
           </ul>
         </div>
